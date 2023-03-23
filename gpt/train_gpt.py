5ce78fc1cfbf9c190f48ac9b09231231a5d0cfd7
4N3MONE <bbangggo123@naver.com>
2023-03-23 22:46:23 +0900

    .

--- gpt/train_gpt.py
+++ gpt/train_gpt.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 import pandas as pd
 import argparse
 
+
 def gpt_train(args):
     # model  = AutoModelForCausalLM.from_pretrained(args.model_checkpoint)
     # tokenizer = AutoTokenizer.from_pretrained(args.model_checkpoint, use_fast=True, stride=128)
@@ -18,11 +19,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Hyperparameter for Training gpt-based model for conditional generation')
     parser.add_argument('--model_checkpoint', default='skt/ko-gpt-trinity-1.2B-v0.5', type=str,
                         help='huggingface model name to train')
-    parser.add_argument('--max_input_length', default=100, type=int,
+    parser.add_argument('--max_input_length', default=30, type=int,
                         help='max input length for dialog')
-    parser.add_argument('--max_target_length', default=100, type=int,
+    parser.add_argument('--max_target_length', default=30, type=int,
                     help='max target length for dialog')
-    parser.add_argument('--train_batch_size', default=8, type=int,
+    parser.add_argument('--train_batch_size', default=4, type=int,
                                             help='train batch size')
     parser.add_argument('--num_train_epochs', default=1, type=int,
                                             help='train epoch size')
@@ -32,7 +33,7 @@ if __name__ == '__main__':
                                             help='weight decay for training'),
     parser.add_argument('--model_name', default='skt/ko-gpt-trinity-1.2B-v0.5', type=str,
                                             help='model name for saving')
-    parser.add_argument('--base_path', default='../data/data_final.tsv', type=str,
+    parser.add_argument('--base_path', default='../data/data_test.tsv', type=str,
                                                 help='dataset path')
     parser.add_argument('--model_path', default='./output', type=str,
                                                 help='model path for saving')
@@ -46,11 +47,10 @@ if __name__ == '__main__':
     model = AutoModelForCausalLM.from_pretrained(args.model_checkpoint)
     tokenizer = AutoTokenizer.from_pretrained(args.model_checkpoint, use_fast=True, stride=128)
     data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False,return_tensors='pt')
-    # data_collator = ChatDataLoader.collate_fn
-    
+
+
     training_args = TrainingArguments(
         output_dir = args.model_path,
-        evaluation_strategy='epoch',
         save_total_limit=1,
         num_train_epochs=args.num_train_epochs,
         per_device_train_batch_size=args.train_batch_size,
@@ -60,17 +60,16 @@ if __name__ == '__main__':
         warmup_steps=3000,
         logging_steps=1000,
         save_strategy="epoch",
-        load_best_model_at_end=True,
-        metric_for_best_model='loss',
-        greater_is_better=False     
+        load_best_model_at_end=False, 
     )
     
     trainer = Trainer(
         model = model,
         args=training_args,
         train_dataset=tokenized_dataset,
+        eval_dataset=None,
         data_collator=data_collator,
-        tokenizer=tokenizer
+        tokenizer=tokenizer,
     )
     # Training
     print('Start Training...')  
