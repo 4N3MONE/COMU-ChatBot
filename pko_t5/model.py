66c29b8b3009b52ffac77277fc7ee5e036ed41ed
minalang <73943090+minalang@users.noreply.github.com>
2023-04-06 14:19:41 +0900

    Delete model.py

--- pko_t5/model.py
+++
@@ -1,8 +0,0 @@
-# Importing the T5 modules from huggingface/transformers
-from transformers import T5ForConditionalGeneration ,T5TokenizerFast
-
-def init_model(model_name):
-    #model_name = 'paust/pko-t5-large'
-    tokenizer = T5TokenizerFast.from_pretrained(model_name)
-    model = T5ForConditionalGeneration.from_pretrained(model_name)
-    return tokenizer, model
